fb056768f6c27260656587c043d2c5e7db10f2a1
Maya <ghamirmanal@gmail.com>
2022-07-04 17:07:07 -0700

    last edit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-
-/*
+/**
  *  * main - Entry point
  *   *
- *    * Return: Always 0 (Success)
-      */
+ *    * Return: always 0
+   */
 int main(void)
 {
-		char test_str[] = "\"Programming is like building a multilingual puzzle";
-
-			puts(test_str);
-				return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
