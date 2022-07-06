91097886387f6875d564e319197fd90203ae82b0
Maya <ghamirmanal@gmail.com>
2022-07-06 15:05:37 -0700

    more

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,6 +7,8 @@ void print_alphabet(void);
 void print_alphabe_x10(void);
 int _islower(int c);
 int _isalpha (int c);
-
+int _abs(int n);
+int print_sign(int n);
+int print_last_digit(int n);
 #endif
 
