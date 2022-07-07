4c91f62f9ba1080d33d457beff47ab5085e4f4e1
Maya <ghamirmanal@gmail.com>
2022-07-06 19:08:58 -0700

    last

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,21 +4,17 @@
  *  * main - to print _putchar   
  *       * Return: (0) if successful
  */
-
 int main(void)
 {
-		char var[] = "_putchar";
-			int ii;
-				/*length of the array var*/
-				int lenVar = sizeof(var) / sizeof(var[0]);
-
-					for (ii = 0; ii < lenVar - 1; ii++)
-							{
-										_putchar(var[ii]);
-										}
-							_putchar('\n');
-
-								return (0);
+char var[] = "_putchar";
+int ii;
+int lenVar = sizeof(var) / sizeof(var[0]);
+for (ii = 0; ii < lenVar - 1; ii++)
+{
+_putchar(var[ii]);
+}
+_putchar('\n');
+return (0);
 }
 	
 
